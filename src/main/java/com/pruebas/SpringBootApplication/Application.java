462f9473f476fc0ddf14aa886b054a677225bd09
Eduardo Esparza <lalopenaesparza@gmail.com>
2022-11-28 21:39:55 -0600

    first-commit

---
+++ src/main/java/com/pruebas/SpringBootApplication/Application.java
@@ -0,0 +1,25 @@
+package com.pruebas.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+	
+	public static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		logger.info("its a continous integration job");
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("its my second logger xd");
+	}
+
+}
